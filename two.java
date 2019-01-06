c1733c0d21f55c44b3de8849a2347fbe702a6a8f
kantharhuban <r.kantharhuban@gmail.com>
2019-01-06 15:56:59 +0530

    two

---
+++ two.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class two
+	{
+			public static void main(String args[])
+				{
+					Scanner s=new Scanner(System.in);
+					int n=s.nextInt();
+					if(n%2==0)
+					{
+						System.out.println("even");
+					}
+					if(n%2!=0)
+					{
+						System.out.println("odd");
+					}
+					else
+					{
+						system.out.println("invalid");
+					}
+				}
+	}
