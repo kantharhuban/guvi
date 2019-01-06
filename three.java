fc8301327df34c33a06f787ed578e29ffbf5cc06
kantharhuban <r.kantharhuban@gmail.com>
2019-01-06 16:17:47 +0530

    three

---
+++ three.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class three
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String n=s.nextLine();
+		if(n.equals("a")||n.equals("e")||n.equals("i")||n.equals("o")||n.equals("u"))
+		{
+		    System.out.println("vowels");
+		}
+		else 
+		{
+		    System.out.println("consonent")
+		}
+	}
+}
